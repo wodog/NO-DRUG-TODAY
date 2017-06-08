1ee37984ef5ba5a998f6eb5112bfabac7720706b
wodog <536505032@qq.com>
2017-06-08 14:01:04 +0800

    update

--- main.go
+++ main.go
@@ -1,67 +1,68 @@
 package main
 
 import (
-  "os"
-  "os/exec"
-  "log"
-  "time"
-  "path/filepath"
-  "github.com/metakeule/fmtdate"
-  "github.com/robfig/cron"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+
+	"github.com/metakeule/fmtdate"
+	"github.com/robfig/cron"
 )
 
-
 func main() {
-  c := cron.New()
-  c.AddFunc("0 22 17 * * *", drug)
-  c.Start()
-  select{}
+	ch := make(chan int)
+	c := cron.New()
+	c.AddFunc("0 22 17 * * *", drug)
+	c.Start()
+	<-ch
 }
 
 func drug() {
-  write()
-  add()
-  commit()
-  push()
+	write()
+	add()
+	commit()
+	push()
 }
 
-func write(){
-  dir, _ := os.Getwd()
+func write() {
+	dir, _ := os.Getwd()
 	filePath := filepath.Join(dir, "README.md")
-  file, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0644)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer file.Close()
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-  file.Write([]byte(getMessage()))
+	file.Write([]byte(getMessage()))
 }
 
 func getMessage() string {
-  date := fmtdate.Format("YYYY-MM-DD", time.Now())
-  return date + ": NO DRUG TODAY!\n\n"
+	date := fmtdate.Format("YYYY-MM-DD", time.Now())
+	return date + ": NO DRUG TODAY!\n\n"
 }
 
 func add() {
-  cmd := exec.Command("git", "add", ".")
-  err := cmd.Run()
-  if err != nil {
-    log.Fatal(err)
-  }
+	cmd := exec.Command("git", "add", ".")
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func commit() {
-  cmd := exec.Command("git", "commit", "-m", "add")
-  err := cmd.Run()
-  if err != nil {
-    log.Fatal(err)
-  }
+	cmd := exec.Command("git", "commit", "-m", "add")
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func push() {
-  cmd := exec.Command("git", "push", "origin", "master:master")
-  err := cmd.Run()
-  if err != nil {
-    log.Fatal(err)
-  }
+	cmd := exec.Command("git", "push", "origin", "master:master")
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
