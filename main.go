fce1225c5b4cf89aacc8b21214015c5ddf326a32
wodog <536505032@qq.com>
2018-02-09 23:38:07 +0800

    add daemon program

--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
-import "time"
+import (
+	"log"
+	"time"
+
+	"github.com/sevlyar/go-daemon"
+)
 
 func main() {
+	context := &daemon.Context{
+		PidFileName: "pid",
+		PidFilePerm: 0644,
+		LogFileName: "log",
+		LogFilePerm: 0640,
+		WorkDir:     "./",
+		Umask:       027,
+		Args:        []string{"[go-daemon sample]"},
+	}
+	p, err := context.Reborn()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if p != nil {
+		return
+	}
+
 	drug()
 	ticker := time.NewTicker(24 * time.Hour)
 	for _ = range ticker.C {
