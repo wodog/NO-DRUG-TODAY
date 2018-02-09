972a0d2e3f396338d992727e601e0ac8b2ee0e25
wodog <536505032@qq.com>
2018-02-10 00:29:25 +0800

    add

--- drug.go
+++ drug.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"github.com/wodog/NO-DRUG-TODAY/git"
+	"fmt"
 )
 
 func drug() {
 	git.Pull()
+	fmt.Println(1)
 	write()
+	fmt.Println(2)
 	git.Add()
+	fmt.Println(3)
 	git.Commit()
+	fmt.Println(4)
 	git.Push()
+	fmt.Println(5)
 }
